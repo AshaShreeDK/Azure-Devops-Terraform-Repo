a224940041bb9d4ec267747d392283ce22f85da1
AshaShreeDK <dlrashadk@gmail.com>
2025-01-15 15:49:26 +0530

    Add files via upload

---
+++ prod.tfvars
@@ -0,0 +1,7 @@
+application_name = "Java-application-PROD"
+location         = "East US"
+java_cidr        = ["192.168.0.0/16"]
+java_sub_cidr    = ["192.10.10.0/24"]
+vm_size          = "Standard_B2s"
+vm_pass          = "Dlradmin1234"
+vm_username      = "dlradmin"
---
+++ providers.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "3.9.0"
+    }
+  }
+  backend "azurerm" {
+    resource_group_name  = "AshaRG"
+    storage_account_name = "dkterraformstate"
+    container_name       = "terraformstatefile"
+    key                  = "terraform.tfstate"
+  }
+
+}
+
+provider "azurerm" {
+  # Configuration options
+  features {}
+
+}
